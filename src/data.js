cf878ded062fdfe6752c19f19ec0dad6105a48fe
Attila Bagossy <bagossyattila@outlook.com>
2020-10-04 22:13:07 +0200

    feat: added repository layout

--- src/data.js
+++ src/data.js
@@ -2,6 +2,10 @@ module.exports = {
   maintainers: {
     BeardOfDoom: {
       name: 'Vécsi Ádám',
+      email: 'vecsi96@gmail.com',
+      github: 'https://github.com/BeardOfDoom/',
+      linkedin: 'https://www.linkedin.com/in/%C3%A1d%C3%A1m-v%C3%A9csi-74a315b3/',
+      image: 'https://avatars1.githubusercontent.com/u/10923698?s=460&u=a5ba4cba611483c9f4c44ef449b00095053c5101&v=4',
     },
     VGeorgee: {
       name: 'Vereb György',
@@ -11,28 +15,36 @@ module.exports = {
     },
     battila7: {
       name: 'Bagossy Attila',
+      email: 'bagossyattila@outlook.com',
+      github: 'https://github.com/battila7/',
+      linkedin: 'https://www.linkedin.com/in/attila-bagossy/',
+      image: 'https://avatars0.githubusercontent.com/u/11032092?s=460&u=313979e5b06ac5c5c0a5c890950922ef862fad0a&v=4',
     },
   },
   repositories: [
     {
       owner: 'cryptid-org',
-      project: 'crpytid-native',
-      description: '',
+      project: 'cryptid-native',
+      description:
+        'A CryptID egy olyan kriptográfiai könyvtár, mely újszerű titkosítási és aláírási protokollokat valósít meg. Mindezt feltörekvő technológiákra alapozva teszi, mint a WebAssembly és a WebAssembly System Interface.',
       maintainers: ['BeardOfDoom', 'battila7'],
+      topics: ['Kriptográfia', 'WebAssembly', 'C'],
       issues: [
         {
-          number: '1',
-          title: '',
-          description: '',
-          level: 1,
+          number: '27',
+          title: 'Windows Build',
+          description:
+            'A könyvtár platformfüggő részeinek portolása Windows-ra, majd a fordítás és futtatás Windows-on.',
+          level: 2,
         },
       ],
     },
-    {
+    /*{
       owner: 'cryptid-org',
       project: 'crpytid-ja',
       description: '',
       maintainers: ['BeardOfDoom', 'battila7'],
+      topics: [],
       issues: [
         {
           number: '1',
@@ -47,6 +59,7 @@ module.exports = {
       project: 'fastify-status',
       description: '',
       maintainers: ['lajoskvcs', 'battila7'],
+      topics: [],
       issues: [
         {
           number: '1',
@@ -61,6 +74,7 @@ module.exports = {
       project: 'sokutei',
       description: '',
       maintainers: ['VGeorgee', 'battila7'],
+      topics: [],
       issues: [
         {
           number: '1',
@@ -69,6 +83,6 @@ module.exports = {
           level: 1,
         },
       ],
-    },
+    },*/
   ],
 }
---
+++ src/index.js
@@ -0,0 +1,114 @@
+const path = require('path')
+const fs = require('fs')
+
+const OUTPUT_DIRECTORY = path.join(__dirname, '..', 'public')
+
+const data = require('./data')
+
+;(function main() {
+  const page = html()
+
+  fs.writeFileSync(path.join(OUTPUT_DIRECTORY, 'index.html'), page)
+
+  fs.copyFileSync(path.join(__dirname, 'main.css'), path.join(OUTPUT_DIRECTORY, 'main.css'))
+})()
+
+function html() {
+  return `
+    <!doctype html>
+    <html lang="hu-HU">
+      ${head()}
+      ${body()}
+    </html>
+    `
+}
+
+function head() {
+  return `
+    <head>
+        <title>Step into Open Source</title>
+
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet"> 
+
+        <link rel="stylesheet" href="main.css" />
+    </head>`
+}
+
+function body() {
+  return `
+    <body>
+        <p>Hai</p>
+
+        <main>
+            <div class="content">
+              <div>
+                <h3>Hacktoberfest Issue-k</h3>
+              </div>
+
+              ${data.repositories.map(repository).join('')}
+            </div>
+        </main>
+    </body>`
+}
+
+function repository(repo) {
+  return `
+        <div class="repository">
+          <div class="repository-title">
+            <h4>
+                <a href="https://github.com/${repo.owner}/${repo.project}">${repo.owner}/${repo.project}</a>
+            </h4>
+          </div>
+          <div class="repository-topics">
+            ${repo.topics.map((t) => `<span>${t}</span>`).join('')}
+          </div>
+          <div class="repository-description">
+            <p>${repo.description}</p>
+          </div>
+          <div class="repository-maintainers">
+            <h5>Akiktől segítséget kérhetsz</h5>
+            ${repositoryMaintainerList(repo)}
+          </div>
+        </div>
+    `
+}
+
+function repositoryMaintainerList(repo) {
+  const maintainers = repo.maintainers.map((username) => {
+    return Object.assign({}, data.maintainers[username], { username })
+  })
+
+  return `
+    <div class="repository-maintainer-list">
+        ${maintainers
+          .map((m) => {
+            return `
+                <div class="repository-maintainer">
+                    <div class="repository-maintainer-image">
+                      <img src="${m.image}" />
+                    </div>
+                    <div class="repository-maintainer-information">
+                      <div class="repository-maintainer-top">
+                        <div class="repository-maintainer-name">
+                            ${m.name}
+                        </div>
+                        <div class="repository-maintainer-email">
+                            ${m.email}
+                        </div>
+                      </div>
+                      <div class="repository-maintainer-bottom">
+                        <a href="${m.github}">
+                          
+                        </a>
+                        <a href="${m.linkedin}">
+                          
+                        </a>
+                      </div>
+                    </div>
+                </div>
+            `
+          })
+          .join('')}
+    </div>
+    `
+}
