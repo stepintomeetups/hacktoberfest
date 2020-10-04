b808b6c22a3d62a843d46366b4ea2a67a48eb645
Attila Bagossy <bagossyattila@outlook.com>
2020-10-04 20:31:36 +0200

    feat: created the barebones data model

---
+++ src/data.js
@@ -0,0 +1,74 @@
+module.exports = {
+  maintainers: {
+    BeardOfDoom: {
+      name: 'Vécsi Ádám',
+    },
+    VGeorgee: {
+      name: 'Vereb György',
+    },
+    lajoskvcs: {
+      name: 'Kovács Lajos',
+    },
+    battila7: {
+      name: 'Bagossy Attila',
+    },
+  },
+  repositories: [
+    {
+      owner: 'cryptid-org',
+      project: 'crpytid-native',
+      description: '',
+      maintainers: ['BeardOfDoom', 'battila7'],
+      issues: [
+        {
+          number: '1',
+          title: '',
+          description: '',
+          level: 1,
+        },
+      ],
+    },
+    {
+      owner: 'cryptid-org',
+      project: 'crpytid-ja',
+      description: '',
+      maintainers: ['BeardOfDoom', 'battila7'],
+      issues: [
+        {
+          number: '1',
+          title: '',
+          description: '',
+          level: 1,
+        },
+      ],
+    },
+    {
+      owner: 'chreeio',
+      project: 'fastify-status',
+      description: '',
+      maintainers: ['lajoskvcs', 'battila7'],
+      issues: [
+        {
+          number: '1',
+          title: '',
+          description: '',
+          level: 1,
+        },
+      ],
+    },
+    {
+      owner: 'sokuteihq',
+      project: 'sokutei',
+      description: '',
+      maintainers: ['VGeorgee', 'battila7'],
+      issues: [
+        {
+          number: '1',
+          title: '',
+          description: '',
+          level: 1,
+        },
+      ],
+    },
+  ],
+}
